2136d00a4e2feaf5a11bec4dab2319a723ecb9d7
Bill Keller <bkeller99@gmail.com>
2019-05-06 17:41:10 -0500

    fixed amount math error

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -53,7 +53,7 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({ error: ''}));
       this.props.onSubmit({
         description: this.state.description,
-        amount: parseFloat(this.state.amount, 10),
+        amount: parseFloat(this.state.amount * 100, 10),
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
       })
