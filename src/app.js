a8462cbd7c94729e6b91bd211194e4a7c93e8634
Bill Keller <bkeller99@gmail.com>
2019-05-17 20:49:18 -0500

    Setup test database env

--- src/app.js
+++ src/app.js
@@ -13,18 +13,6 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.subscribe (() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  // console.log(visibleExpenses);
-})
-
-store.dispatch(addExpense({description: 'water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'electric bill', amount: 6000}));
-store.dispatch(addExpense({description: 'gas bill', amount: 4000, createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 109500}));
-
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,124 +2,126 @@ import * as firebase from 'firebase';
   
   // Your web app's Firebase configuration
  const firebaseConfig = {
-    apiKey: "AIzaSyCTN4HZDgFUgN23FF2gr_Z0E4WH8TSnhPA",
-    authDomain: "expensify-8964c.firebaseapp.com",
-    databaseURL: "https://expensify-8964c.firebaseio.com",
-    projectId: "expensify-8964c",
-    storageBucket: "expensify-8964c.appspot.com",
-    messagingSenderId: "281700581801",
-    appId: "1:281700581801:web:6edc6c039f108fdc"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
   const database = firebase.database();
 
-  database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+  export {firebase, database as default};
 
-  database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+//   database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
 
-  database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+//   database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
 
-  // database.ref('expenses')
-  //   .on('value', (snapshot) => {
-  //     const expenses = [];
+//   database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
 
-  //     snapshot.forEach((childSnapshot) => {
-  //       expenses.push({
-  //         id: childSnapshot.key,
-  //         ...childSnapshot.val()
-  //       });
-  //     });
-  //     console.log(expenses);
-  //   });
+//   // database.ref('expenses')
+//   //   .on('value', (snapshot) => {
+//   //     const expenses = [];
 
- // database.ref().on('value', (snapshot) => {
-  //   console.log(snapshot.val());
-  // });
+//   //     snapshot.forEach((childSnapshot) => {
+//   //       expenses.push({
+//   //         id: childSnapshot.key,
+//   //         ...childSnapshot.val()
+//   //       });
+//   //     });
+//   //     console.log(expenses);
+//   //   });
 
-    // database.ref('expenses')
-    //   .once('value')
-    //   .then((snapshot) => {
-    //     const expenses = [];
+//  // database.ref().on('value', (snapshot) => {
+//   //   console.log(snapshot.val());
+//   // });
+
+//     // database.ref('expenses')
+//     //   .once('value')
+//     //   .then((snapshot) => {
+//     //     const expenses = [];
   
-    //     snapshot.forEach((childSnapshot) => {
-    //       expenses.push({
-    //         id: childSnapshot.key,
-    //         ...childSnapshot.val()
-    //       });
-    //     });
-    //     console.log(expenses);
-    //   });
+//     //     snapshot.forEach((childSnapshot) => {
+//     //       expenses.push({
+//     //         id: childSnapshot.key,
+//     //         ...childSnapshot.val()
+//     //       });
+//     //     });
+//     //     console.log(expenses);
+//     //   });
 
-  // database.ref('expenses').push({
-  //   description: 'gas bill',
-  //   note: ' ',
-  //   amount: 3000,
-  //   createdAt: 984723987
-  // })
-  // database.ref('expenses').push({
-  //   description: 'phone bill',
-  //   note: ' ',
-  //   amount: 4000,
-  //   createdAt: 489237287
-  // })
-  // database.ref('expenses').push({
-  //   description: 'coffee',
-  //   note: 'with Bob',
-  //   amount: 150,
-  //   createdAt: 92407823907
-  // })
+//   // database.ref('expenses').push({
+//   //   description: 'gas bill',
+//   //   note: ' ',
+//   //   amount: 3000,
+//   //   createdAt: 984723987
+//   // })
+//   // database.ref('expenses').push({
+//   //   description: 'phone bill',
+//   //   note: ' ',
+//   //   amount: 4000,
+//   //   createdAt: 489237287
+//   // })
+//   // database.ref('expenses').push({
+//   //   description: 'coffee',
+//   //   note: 'with Bob',
+//   //   amount: 150,
+//   //   createdAt: 92407823907
+//   // })
 
-  // database.ref('notes/-Lf6Tu0_aS5EIQurNtD6').update({
-  //   title: 'To Do'
-  // })
+//   // database.ref('notes/-Lf6Tu0_aS5EIQurNtD6').update({
+//   //   title: 'To Do'
+//   // })
 
-  // database.ref('notes').push({
-  //   title: 'course topics',
-  //   body: 'java, js, ts'
-  // })
+//   // database.ref('notes').push({
+//   //   title: 'course topics',
+//   //   body: 'java, js, ts'
+//   // })
 
 
-  // database.ref('location/city')
-  //   .once('value')
-  //   .then((snapshot) => {
-  //     const val = snapshot.val();
-  //     console.log(val);
-  //   })
-  //   .catch((e) => {
-  //     console.log('error fetching data: ', e);
-  //   })
+//   // database.ref('location/city')
+//   //   .once('value')
+//   //   .then((snapshot) => {
+//   //     const val = snapshot.val();
+//   //     console.log(val);
+//   //   })
+//   //   .catch((e) => {
+//   //     console.log('error fetching data: ', e);
+//   //   })
 
-  // database.ref().set({
-  //   name: 'Andrew Mead',
-  //   age: 26,
-  //   stressLevel: 6,
-  //   job: {
-  //     title: 'developer',
-  //     company: 'google'
-  //   },
-  //   location: {
-  //     city: 'philly',
-  //     country: 'usa'
-  //   }
-  // }).then(() => {
-  //   console.log('data is saved!');
-  // }).catch((e) => {
-  //   console.log('this failed ', e);
-  // })
+//   // database.ref().set({
+//   //   name: 'Andrew Mead',
+//   //   age: 26,
+//   //   stressLevel: 6,
+//   //   job: {
+//   //     title: 'developer',
+//   //     company: 'google'
+//   //   },
+//   //   location: {
+//   //     city: 'philly',
+//   //     country: 'usa'
+//   //   }
+//   // }).then(() => {
+//   //   console.log('data is saved!');
+//   // }).catch((e) => {
+//   //   console.log('this failed ', e);
+//   // })
 
-  // database.ref().update({
-  //   stressLevel: 9,
-  //   'job/company': 'amazon',
-  //   'location/city': 'seattle'
-  // })
+//   // database.ref().update({
+//   //   stressLevel: 9,
+//   //   'job/company': 'amazon',
+//   //   'location/city': 'seattle'
+//   // })
 
-  // database.ref().remove();
+//   // database.ref().remove();
 
